bb436917d07febd1b241729362993db6858f395d
greenRose2018 <brifun13@gmail.com>
2017-12-07 08:06:30 -0700

    Created a path to check for image

--- BrittneyMorales_CSP/Controller/CreativityController.swift
+++ BrittneyMorales_CSP/Controller/CreativityController.swift
@@ -30,6 +30,37 @@ class CreativityController: UICollectionViewController, UICollectionViewDelegate
             UIImage(named: "tiny pikachu")
         ]
     }
+    
+    var largePhotoIndexPath: IndexPath?
+    {
+        didSet
+        {
+            var indexPaths = [IndexPath]()
+            if let largePhotoIndexPath = largePhotoIndexPath
+            {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue
+            {
+                indexPaths.append(oldValue)
+            }
+            
+            collectionView?.performBatchUpdates(
+                {
+                self.collectionView?.reloadItems(at: indexPaths)
+                })
+            {
+                completed in
+                
+                if let largePhotoIndexPath = self.largePhotoIndexPath
+                {
+                    self.collectionView?.scrollToItem(at: largePhotoIndexPath,
+                                                      at: .centeredVertically,
+                                                      animated: true)
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
