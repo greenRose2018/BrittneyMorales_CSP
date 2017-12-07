ab48d689ca20a2c08f48805bb2afcd2b5645121d
greenRose2018 <brifun13@gmail.com>
2017-12-07 08:04:46 -0700

    declare and intialized data members and lazy  variable

--- BrittneyMorales_CSP/Controller/CreativityController.swift
+++ BrittneyMorales_CSP/Controller/CreativityController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
-class CreativityController: UIViewController {
-
+class CreativityController: UICollectionViewController, UICollectionViewDelegateFlowLayout
+{
+    
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
+    
+    private lazy var artSelection :[UIImage?] =
+    {
+     
+        return [
+            UIImage(named: "Haiku"),
+            UIImage(named: "drawing"),
+            UIImage(named: "tiny pikachu"),
+            UIImage(named: "tiny pikachu"),
+            UIImage(named: "tiny pikachu"),
+            UIImage(named: "Haiku"),
+            UIImage(named: "Haiku"),
+            UIImage(named: "drawing"),
+            UIImage(named: "tiny pikachu")
+        ]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
