693670f80539a58dee94e939b76208038aa5ed2b
greenRose2018 <brifun13@gmail.com>
2017-12-07 09:51:30 -0700

    create file and link componets

---
+++ BrittneyMorales_CSP/Controller/ArtCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCellCollectionViewCell.swift
+//  BrittneyMorales_CSP
+//
+//  Created by Morales, Brittney on 12/7/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageName: UILabel!
+    
+}
