2322bec84f2b0f8d43e63a456c60e24dcd0cb7ee
greenRose2018 <brifun13@gmail.com>
2017-12-19 09:03:24 -0700

    created DeathScene file

---
+++ BrittneyMorales_CSP/Controller/GameController/DeathScene.swift
@@ -0,0 +1,14 @@
+//
+//  DeathScene.swift
+//  BrittneyMorales_CSP
+//
+//  Created by Morales, Brittney on 12/19/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class DeathScene: SKScene
+{
+}
