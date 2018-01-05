6082e8d19d4a921bb1daf1d7a91b19545f994403
greenRose2018 <brifun13@gmail.com>
2018-01-05 09:57:42 -0700

    misplaced method in wrong controller moved to BucketItemCell

--- BrittneyMorales_CSP/Controller/DataView/DataViewController.swift
+++ BrittneyMorales_CSP/Controller/DataView/DataViewController.swift
@@ -8,57 +8,34 @@
 
 import UIKit
 
-class DataViewController: UITableViewController {
-
-    @IBOutlet weak var bucketItemText: UILabel!
-    @IBOutlet weak var bucketItemSymbol: UILabel!
-    @IBOutlet weak var bucketItemSignature: UILabel!
-    
-    var bucketItem : BucketItem!
-    {
-        didSet
+class DataViewController: UITableViewController
+{
+    lazy  var bucketList : [BucketItem] =
         {
-            updateCellView()
-        }
-    }
+            return loadBucketListFromFile()
+    }()
     
-    private func randomEmoji() -> String
+    private func loadBucketListFromFile() -> [BucketItem]
     {
-        let emojiStart = 0x1F601
-        let emojiEnd = 0x1F64F
-        let symbolStart = 0x1F680
-        let symbolEnd = 0x1F6C5
-        
-        let emojiRange = 79
-        let symbolRange = 70
-        
-        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
-        let emoji = UnicodeScalar(ascii)?.description
-        return emoji!
-    }
-    
-    private func updateCellView() -> Void
-    {
-        if (bucketItem != nil)
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource: "bucket", withExtension: "csv")
         {
-            bucketItemSignature.text = bucketItem.itemAuthor
-            bucketItemText.text = bucketItem.itemContents
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: ",")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0],author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File load error")
+            }
         }
-        else
-        {
-            bucketItemSignature.text = "author goes here"
-            bucketItemText.text = "text goes here"
-        }
-        bucketItemSymbol.text = randomEmojiSymbol()
-    }
-    
-    public override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
-    public override func setSelected(_ selected: Bool, animated: Bool)
-    {
-        super.setSelected(selected, animated: animated)
+        return items
     }
     
     override func viewDidLoad() {
@@ -71,76 +48,24 @@ class DataViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return bucketList.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
 
-        // Configure the cell...
+        currentCell.bucketItem = bucketList[indexPath.row]
 
-        return cell
+        return currentCell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
