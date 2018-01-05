9e0628bb836b6a7a9cbb2da8bfef1443049ab9ea
greenRose2018 <brifun13@gmail.com>
2018-01-05 09:42:53 -0700

    added contents and method to BucketItem

--- BrittneyMorales_CSP/Model/BucketItem.swift
+++ BrittneyMorales_CSP/Model/BucketItem.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+public class BucketItem
+{
+    let itemContents: String
+    let itemAuthor : String
+    
+    init(contents: String, author: String)
+    {
+        itemContents = contents
+        itemAuthor = author
+    }
+}
